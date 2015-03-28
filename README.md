1b415501d45771ab2e6e993b9b011f0b70f1ec5c
Michael Kreil <github@michael-kreil.de>
2015-03-28 16:27:20 +0100

    Merge remote-tracking branch 'origin/master'
    Conflicts:
    web/script/main.js

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # ripe-map
 Map for RIPE
+
+## Probe Status
+ * 0 - not active (recently bought)
+ * 1 - connected
+ * 2 - disconnected
+ * 3 - abadonned 
--- web/script/main.js
+++ web/script/main.js
@@ -1,9 +1,9 @@
-
 $(function () {
+
 	var map = L.map('map', {zoomAnimation:false});
-	//map.fitWorld();
+	map.fitWorld();
 	//map.setZoom(map.getZoom()+);
-	map.setView([50,10], 5);
+	//map.setView([50,10], 5);
 
 	// add an OpenStreetMap tile layer
 	L.tileLayer('http://odcdn.de:7772/europe2/{z}/{x}/{y}.png', {
@@ -24,11 +24,60 @@ $(function () {
 		})
 
 		probes.forEach(function (probe) {
-			probe.fillColor   = (['#eee','#8e8','#e88','#eee'])[probe.status];
-			probe.strokeColor = (['#888','#484','#844','#888'])[probe.status];
+			setColors(probe)
+			//probe.fillColor = (['#eee', '#8e8', '#e88', '#eee'])[probe.status];
+			//probe.strokeColor = (['#888', '#484', '#844', '#888'])[probe.status];
 			//probe.r = 3;
 		});
 
 		canvasLayer.setPoints(probes);
-	})
+	});
+
+
+	var socket = io("https://atlas-stream.ripe.net:443", {path: "/stream/socket.io"});
+
+	socket.on("atlas_probestatus", function (event, err) {
+		if (err) console.log(err);
+		console.log("I received ");
+		console.log(event);
+
+	});
+
+	socket.on("connect", function (con) {
+		//socket.emit("atlas_subscribe", {stream_type: "probestatus", sendBacklog: true});
+		socket.emit('atlas_subscribe', {stream_type: "probestatus"});
+
+		// Do something when the subscription has been terminated
+		socket.on("atlas_unsubscribed", function (what) {
+			console.log("The client has been unsubscribed from " + what);
+		});
+
+		socket.on("atlas_error", function (err) {
+			console.log(err);
+		});
+
+	});
+
+	function setColors(probe) {
+		switch (probe.status) {
+			case 0 :
+				probe.fillColor = 'rgba(255,215,0, 0.5)';
+				probe.strokeColor = 'rgba(255,215,0, 0.8)';
+				break;
+			case 1:
+				probe.fillColor = 'rgba(111,200,51, 0.5)';
+				probe.strokeColor = 'rgba(111,200,51, 0.8)';
+				break;
+			case 2:
+				probe.fillColor = 'rgba(189,31,48, 0.5)';
+				probe.strokeColor = 'rgba(189,31,48, 0.8)';
+				break;
+
+			case 3:
+				probe.fillColor = 'rgba(98,98,98, 0.5)';
+				probe.strokeColor = 'rgba(98,98,98, 0.5)';
+				break;
+		}
+	}
+
 })
