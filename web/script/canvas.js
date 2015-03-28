91ae3fc0d768e158d503b6c3cc068a8a6a357e02
Michael Kreil <github@michael-kreil.de>
2015-03-28 14:01:00 +0100

    showing probes

--- web/script/canvas.js
+++ web/script/canvas.js
@@ -5,28 +5,22 @@ function CanvasLayer (map) {
 
 	var markers = [];
 
-	for (var i = 0; i < 1000; i++) {
-		var r = Math.random()*200;
-		var g = Math.random()*200;
-		var b = Math.random()*200;
-
-		markers.push({
-			lat: (Math.random()-0.5)*170,
-			lng: (Math.random()-0.5)*360,
-			fillColor:   'rgb('+Math.round(r    )+','+Math.round(g    )+','+Math.round(b    )+')',
-			strokeColor: 'rgb('+Math.round(r*0.5)+','+Math.round(g*0.5)+','+Math.round(b*0.5)+')',
-		})
+	me.setPoints = function (list) {
+		markers = list;
+		markers.forEach(function (marker) { marker.levelPosition = [] });
+		layoutedLevel = [];
+		redraw();
 	}
 
-	markers.forEach(function (marker) { marker.levelPosition = [] });
+	function redraw() {
+		updateFunctions.forEach(function (func) { func() });
+	}
 
 	var layoutedLevel = [];
 
 	var canvasTiles = L.tileLayer.canvas();
 
 	canvasTiles.drawTile = function (canvas, tilePoint, zoom) {
-		if (!layoutedLevel[zoom]) layoutLevel(zoom);
-
 		var size = canvas.width;
 		var ctx = canvas.getContext('2d');
 
@@ -36,30 +30,38 @@ function CanvasLayer (map) {
 		var x1 = x0 + size;
 		var y1 = y0 + size;
 
-		markers.forEach(function (marker) {
-			var point = marker.levelPosition[zoom];
-			var r = point.r;
+		function drawTile () {
+			if (!layoutedLevel[zoom]) layoutLevel(zoom);
 
-			if (point.x + r < x0) return;
-			if (point.x - r > x1) return;
-			if (point.y + r < y0) return;
-			if (point.y - r > y1) return;
+			markers.forEach(function (marker) {
+				var point = marker.levelPosition[zoom];
+				var r = point.r;
 
-			ctx.beginPath();
-			ctx.arc(
-				point.x - x0,
-				point.y - y0,
-				r, 0, Math.PI*2, false);
-			ctx.fillStyle = marker.fillColor;
-			ctx.fill();
-			ctx.strokeStyle = marker.strokeColor;
-			ctx.stroke();
-		})
+				if (point.x + r < x0) return;
+				if (point.x - r > x1) return;
+				if (point.y + r < y0) return;
+				if (point.y - r > y1) return;
+
+				ctx.beginPath();
+				ctx.arc(
+					point.x - x0,
+					point.y - y0,
+					r, 0, Math.PI*2, false);
+				ctx.fillStyle = marker.fillColor;
+				ctx.fill();
+				ctx.strokeStyle = marker.strokeColor;
+				ctx.stroke();
+			})
+		}
+
+		drawTile();
+
+		updateFunctions.push(drawTile);
 	}
 
 	function layoutLevel(zoom) {
 		markers.forEach(function (marker) {
-			var p =  map.project([marker.lat, marker.lng], zoom);
+			var p =  map.project([marker.latitude, marker.longitude], zoom);
 			marker.levelPosition[zoom] = { x0:p.x, y0:p.y, x:p.x, y:p.y, r:3 }
 		})
 	}
--- web/script/main.js
+++ web/script/main.js
@@ -10,4 +10,24 @@ $(function () {
 	}).addTo(map);
 
 	var canvasLayer = new CanvasLayer(map);
+
+	var probes;
+
+	$.getJSON('data/probes.json', function (data) {
+		probes = [];
+		Object.keys(data).forEach(function (key) {
+			data[key].forEach(function (value, index) {
+				if (!probes[index]) probes[index] = {};
+				probes[index][key] = value;
+			})
+		})
+
+		probes.forEach(function (probe) {
+			probe.fillColor   = (['#eee','#8d9','#faa','#eee'])[probe.status];
+			probe.strokeColor = (['#888','#485','#855','#888'])[probe.status];
+			//probe.r = 3;
+		});
+
+		canvasLayer.setPoints(probes);
+	})
 })
